66224ac175936fc36f3da7b075a89d9e866c56e1
Jake Lovett <lovett_jp@outlook.com>
2019-12-22 14:55:05 -0600

    Updates

---
+++ precache-manifest.bb773070ab0fc4aa061a2e7729927a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e176ae58be40c82cb794a3a8d6d1e2b1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "bf38b84fb638d9ae6646",
+    "url": "/react-todo/static/css/main.a8728022.chunk.css"
+  },
+  {
+    "revision": "e84e9affcce896367644",
+    "url": "/react-todo/static/js/2.3bab8f8b.chunk.js"
+  },
+  {
+    "revision": "bf38b84fb638d9ae6646",
+    "url": "/react-todo/static/js/main.ab69fb3f.chunk.js"
+  },
+  {
+    "revision": "3371e6c8ccd1ae67794e",
+    "url": "/react-todo/static/js/runtime-main.8e795b32.js"
+  }
+]);
